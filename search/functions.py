c45dc113bdbee632acd757231e6c271c3cceb5ec
Rob <>
2021-07-29 18:21:59 -0400

    Update functions.py

--- search/functions.py
+++ search/functions.py
@@ -11,7 +11,7 @@ from elasticsearch import Elasticsearch, helpers
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
-from django.core.files.uploadedfile TemporaryUploadedFile
+from django.core.files.uploadedfile import TemporaryUploadedFile
 
 from .models import MiscDocs
 
